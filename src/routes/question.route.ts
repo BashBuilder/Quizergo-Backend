58b749008a10fd2adbcc885f9821a2f2bf6d050c
Bashbuilder <iamadelowotan@gmail.com>
2026-08-15 18:59:09 +0100

    feat: removed api key functionality

--- src/routes/question.route.ts
+++ src/routes/question.route.ts
@@ -1,17 +1,10 @@
 import { Router } from "express";
-import apikeyRouter from "../middleware/apikey.middleware.js";
-import { permissionMiddleware } from "../middleware/permission.middleware.js";
-import { Permissions } from "../generated/prisma/enums.js";
 import requireAuth from "../middleware/auth.middleware.js";
 import * as questionController from "../controllers/questions.controller.js";
 import validateRequest, { ValidationSource } from "../helper/validator.js";
 import { questionQuerySchema } from "../models/subject.model.js";
 
 const questionRoutes: Router = Router();
-
-// questionRoutes.use(apikeyRouter);
-// questionRoutes.use(permissionMiddleware(Permissions.GENERAL));
-// questionRoutes.use(authMiddleware);
 
 questionRoutes.route("/subjects").get(questionController.getSubjectLists);
 
